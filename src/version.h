550eac8708752959e9f9097699f133f9cb63192b
novo-max <novotel8468@proton.me>
2024-06-15 13:50:44 +0000

    just another test

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.2.6"
+        #define VERSION "6.0.0"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2024-03-26 23:18:21.389647"
